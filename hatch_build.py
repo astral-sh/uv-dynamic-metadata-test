6c5aa0a65db737c9e7e2e60dc865bd8087012e64
Charlie Marsh <charlie.r.marsh@gmail.com>
2024-12-02 23:38:14 -0500

    Use dynamic in root

--- hatch_build.py
+++ hatch_build.py
@@ -3,4 +3,5 @@ from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 
 class LiteraryBuildHook(BuildHookInterface):
     def initialize(self, version, build_data):
-        build_data["dependencies"].append("anyio")
+        build_data["dependencies"].append("typing-extensions")
+        build_data["dependencies"].append("dependency")
